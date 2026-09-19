2a1adc449cf77b38a40023e02b4b61565caec3c2
rmohanty521 <rithmohanty07@gmail.com>
2026-09-19 17:24:06 -0400

    notebook 2 progress with tables

---
+++ src/metrics.py
@@ -0,0 +1,34 @@
+import numpy as np
+import pandas as pd
+
+
+def daily_returns(prices):
+    return prices.pct_change()
+
+
+def rolling_vol(returns, window: int = 10):
+    return returns.rolling(window).std() * np.sqrt(252)
+
+
+def ann_vol(returns):
+    return returns.std() * np.sqrt(252)
+
+
+def sharpe(returns):
+    return returns.mean() / returns.std() * np.sqrt(252)
+
+
+def max_drawdown(prices):
+    """Calculates a ratio of the price its on vs the highest price
+    its seen up to that point. Then we subtract 1 from the ratio so
+    it reflects how far away we are from that max. For example, 0.75
+    turns into -0.25 meaning we are -0.25% from the max. Then we do
+    .min to find the worst spot we were in relative to running peak
+    at that point in time.
+
+    """
+    return (prices / prices.cummax() - 1).min()
+
+
+def max_drawup(prices):
+    return (prices / prices.cummin() - 1).max()
